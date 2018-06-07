ea42b9077702708210305134c6d9e7179a89ead4
Lars Nagel <lars.nagel@tum.de>
2018-06-07 16:51:03 +0200

    third
    Halt die Klappe

--- src/Main.java
+++ src/Main.java
@@ -3,11 +3,8 @@ public class Main {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-<<<<<<< HEAD
-		System.out.println("Hello World, du Ficker");
-=======
-		System.out.println("Hello World; Haha Fred");
->>>>>>> c0d83ba7f62492538ea52fba322f80f2b4fcdd2b
+		System.out.println("Hello World, du Ficker; selber du Mongo");
+
 	}
 
 }
